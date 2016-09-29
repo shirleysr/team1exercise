6931b42e7027bda96284b59b50227aaa4dbd3fcb
shirleysr <shirui1203@qq.com>
2016-09-29 13:54:25 +0800

    string转int
    方便比较大小

--- web/team1exercise.dart
+++ web/team1exercise.dart
@@ -2,15 +2,26 @@ import 'dart:html';
 
 void main() {
   querySelector('#sample_text_id')
-    ..text = 'Click me!'
-    ..onClick.listen(reverseText);
+    .. text = "2,5,1,8,7"
+    ..onClick.listen(sort);
 }
 
-void reverseText(MouseEvent event) {
-  var text = querySelector('#sample_text_id').text;
-  var buffer = new StringBuffer();
-  for (int i = text.length - 1; i >= 0; i--) {
-    buffer.write(text[i]);
+  void sort(MouseEvent event){
+    var text = querySelector('#sample_text_id').text;
+    var buffer = new StringBuffer();
+    for (int i = text.length - 1; i >= 0; i--) {
+      buffer.write(text[i]);
+    }
+    int i,j;
+    for(i=2;i<=text.length;++i)
+      if (int.parse(text[i])<int.parse(text[i-1]))
+      {
+      int.parse(text[0])=int.parse(text[i]);
+      int.parse(text[i])=int.parse(text[i-1]);
+        for(j=i-2;int.parse(text[0])<int.parse(text[j]);--j)
+      int.parse(text[j+1])=int.parse(text[0]);
+      }
+    for ( i = 0; i <= text.length; i++) {
+      buffer.write(text[i]);}
+    querySelector('#sample_text_id').text = buffer.toString();
   }
-  querySelector('#sample_text_id').text = buffer.toString();
-}
